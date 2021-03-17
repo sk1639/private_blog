e0b06aaf6c5c3dababc61bb3752814d1b7ac3471
yangju <yangju@gmtc.kr>
2021-03-17 15:09:20 +0900

    add build files

---
+++ client/build/precache-manifest.002e3d077f908c2ffa56f6edb5d23c6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecfb73671434eb960791610f5c533919",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc6e4665cd56b8429d1a",
+    "url": "/static/css/2.b6e3a137.chunk.css"
+  },
+  {
+    "revision": "2d5bed2350be936bc504",
+    "url": "/static/css/main.b2713449.chunk.css"
+  },
+  {
+    "revision": "dc6e4665cd56b8429d1a",
+    "url": "/static/js/2.fce4a318.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.fce4a318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d5bed2350be936bc504",
+    "url": "/static/js/main.4f3b0271.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "9e1efe36a6da96a2bb37ed792fc934ae",
+    "url": "/static/media/generic-avatar.9e1efe36.png"
+  }
+]);
