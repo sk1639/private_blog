ab172615f3969412ce8ce5a5a6c288fd1370102a
yangju <yangju@gmtc.kr>
2021-03-17 14:51:45 +0900

    settting changed

--- server/config/key.js
+++ server/config/key.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
+    console.log('production..connected');
     module.exports = require('./prod');
 } else {
+    console.log('dev..connected');
     module.exports = require('./dev');
 }
